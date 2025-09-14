0197b6c5b2b7dd521bd39f7e85064a165d986b80
Your Name <you@example.com>
2025-09-14 21:57:41 +0530

    express listen the urlencode data of post

--- index.js
+++ index.js
@@ -12,16 +12,33 @@ app.listen(port, () => {
 //     res.send("app Use function is running when any action perform on  server");
 // });
 
+app.use(express.urlencoded({ extended: true })); // ab express samaj  jaye ga ki data url encode hai
+app.use(express.urlencoded(express.json())); // ab express samaj  jaye ga ki row data hai
+
 app.get("/", (req, res) => {
-    res.send("ths is root path");
+    // let { user } = req.query;
+    res.send(`this is root path`);
 });
 
-app.get("/apple", (req, res) => {
-    res.send("This is apple directory");
+
+
+app.get("/new", (req, res) => {           //    http://localhost:8080/new?user="hello"
+    let { user } = req.query;
+    if (!user) {
+        console.log("not found");
+    }
+    console.log("this is user ", user);
+    res.send(`this is user ${user}`);
 });
-app.get("/mango", (req, res) => {
-    res.send("This is mango directory");
+
+
+
+app.post("/userdata", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 });
+
+
 app.get(/.*/, (req, res) => {
-    res.send("This directory is empty or default directory");
+    res.send("This directory is empty or default directory ");
 });
