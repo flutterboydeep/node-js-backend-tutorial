81c396903e52b8b57a7f5f6f30986cc90a918f63
Your Name <you@example.com>
2025-11-01 14:45:32 +0530

    create connection of mysql with node

--- index.js
+++ index.js
@@ -1,118 +1,39 @@
 
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Deep@sql1",
+    database: "test",
+}
+);
 
-const express = require("express");
-const path = require('path');
-const app = express();
-const { v4: uuidv4 } = require('uuid');
-const methodOverride = require('method-override')
-let port = 8080;
-app.listen(port, () => {
-    console.log("the app is running at port ", port);
+let q = "select * from user";
+try {
+    connection.query(q, (err, result) => {
+        if (err) throw err;
+        console.log('result ', result);
 
-});
-app.use(methodOverride('_method'));
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+    });
+    connection.end();
 
-
-
-
-let arr = [
-    {
-        'id': 'jkjkjg',
-        'username': 'Deep',
-        'comment': 'how are you everyone',
-
-    },
-    {
-        'id': 'jkjkjgfsfs',
-        'username': 'Deep',
-        'comment': 'how are you everyone and i am now sleeping',
-
-    }
-];
-
-app.get("/", (req, res) => {
-    res.render("index.ejs", { arr }
-
-    );
-
-});
-app.get("/new", (req, res) => {
-    res.render("form.ejs");
-
-});
-
-app.post("/posts", (req, res) => {
-    let { username, comment } = req.body;
-    const myUUID = uuidv4();
-    let data = {
-        'id': myUUID,
-        'comment': comment,
-        'username': username,
-    }
-    arr.push(data);
-    res.redirect("/");
-
-    console.log(data);
-
-
-});
-
-app.get("/posts/:id/details", (req, res) => {
-    let { id } = req.params;
-
-    let post = arr.find((e) => e.id === id);
-    console.log("id is = ", id);
-    res.render("view_details_post.ejs", { post });
-
-});
-
-app.patch("/posts/:id/edits", (req, res) => {
-    let { id } = req.params;
-    const data = req.body;
-    let post = arr.find((e) => e.id === id);
-    post.comment = req.body.comment;
-    res.redirect('/');
+} catch (e) {
+    console.log(e);
+}
 
 
 
-    console.log(post);
+// console.log(getRandomUser());
 
 
 
 
+// let getRandomUser = () => {..
+//     return {
+//         id: faker.datatype.uuid(),
+//         username: faker.internet.username(),
+//         email: faker.internet.email(),
+//         password: faker.internet.password(),
 
-    // console.log("func tion is running ...",);
-    // res.send("Patch request is working fine");
-
-});
-
-app.get("/posts/:id/edit", (req, res) => {
-    let { id } = req.params;
-    let post = arr.find((e) => e.id === id);
-    res.render('update_form.ejs', { post });
-
-});
-
-app.delete("/posts/:id/delete", (req, res) => {
-    let { id } = req.params;
-    arr = arr.filter((e) => e.id !== id);
-    console.log(arr);
-
-    res.render('index.ejs', { arr });
-
-});
-
-//     console.log(data);S
-
-
-// });
-
-
-
-app.get(/.*/, (req, res) => {
-    res.send("This directory is empty or default directory ");
-});
+//     }
+// }
